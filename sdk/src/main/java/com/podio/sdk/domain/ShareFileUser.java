36f089976191e6ffe0d704da5448a390fae702be
Sai Prajnan Emani <sai.emani@citrix.com>
2017-01-13 14:00:58 -0500

    New ShareFileUser model added.

---
+++ sdk/src/main/java/com/podio/sdk/domain/ShareFileUser.java
@@ -0,0 +1,33 @@
+package com.podio.sdk.domain;
+
+/**
+ * Created by sai on 1/13/17.
+ */
+
+public class ShareFileUser {
+    private final String access_token = null;
+    private final String subdomain = null;
+    private final String apicp = null;
+    private final String appcp = null;
+    private final String refresh_token = null;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public String getSubdomain() {
+        return subdomain;
+    }
+
+    public String getApicp() {
+        return apicp;
+    }
+
+    public String getAppcp() {
+        return appcp;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+}
